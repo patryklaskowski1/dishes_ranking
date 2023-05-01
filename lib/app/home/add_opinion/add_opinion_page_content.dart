ca87b069442a5f6b7702b9b8a30739868c25623e
Patryk Laskowski <patrick_laskowski@hotmail.com>
2023-05-01 20:54:24 +0200

    Change bottom navigation bar index to 0 on Save

--- lib/app/home/add_opinion/add_opinion_page_content.dart
+++ lib/app/home/add_opinion/add_opinion_page_content.dart
@@ -3,8 +3,11 @@ import 'package:flutter/material.dart';
 
 class AddOpinionPageContent extends StatefulWidget {
   const AddOpinionPageContent({
+    required this.onSave,
     Key? key,
   }) : super(key: key);
+
+  final Function onSave;
 
   @override
   State<AddOpinionPageContent> createState() => _AddOpinionPageContentState();
@@ -18,62 +21,68 @@ class _AddOpinionPageContentState extends State<AddOpinionPageContent> {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          TextField(
-            decoration: const InputDecoration(
-              hintText: 'Podaj nazwę restauracji',
+      child: Padding(
+        padding: const EdgeInsets.all(30),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              decoration: const InputDecoration(
+                hintText: 'Podaj nazwę restauracji',
+              ),
+              onChanged: (newValue) {
+                setState(() {
+                  restaurantName = newValue;
+                });
+              },
             ),
-            onChanged: (newValue) {
-              setState(() {
-                restaurantName = newValue;
-              });
-            },
-          ),
-          TextField(
-            decoration: const InputDecoration(
-              hintText: 'Podaj nazwę dania',
+            TextField(
+              decoration: const InputDecoration(
+                hintText: 'Podaj nazwę dania',
+              ),
+              onChanged: (newValue) {
+                setState(
+                  () {
+                    dishName = newValue;
+                  },
+                );
+              },
             ),
-            onChanged: (newValue) {
-              setState(
-                () {
-                  dishName = newValue;
-                },
-              );
-            },
-          ),
-          const SizedBox(height: 20),
-          Slider(
-            onChanged: (newValue) {
-              setState(
-                () {
-                  rating = newValue;
-                },
-              );
-            },
-            value: rating,
-            min: 1.0,
-            max: 6.0,
-            divisions: 10,
-            label: rating.toString(),
-          ),
-          const SizedBox(height: 25),
-          ElevatedButton(
-            onPressed: () {
-              FirebaseFirestore.instance.collection('restaurants').add(
-                {
-                  'name': restaurantName,
-                  'dishes': dishName,
-                  'rating': rating,
-                },
-              );
-            },
-            child: const Text(
-              'DODAJ',
+            const SizedBox(height: 20),
+            Slider(
+              onChanged: (newValue) {
+                setState(
+                  () {
+                    rating = newValue;
+                  },
+                );
+              },
+              value: rating,
+              min: 1.0,
+              max: 6.0,
+              divisions: 10,
+              label: rating.toString(),
             ),
-          )
-        ],
+            const SizedBox(height: 25),
+            ElevatedButton(
+              onPressed: restaurantName.isEmpty || dishName.isEmpty
+                  ? null
+                  : () {
+                      FirebaseFirestore.instance.collection('restaurants').add(
+                        {
+                          'name': restaurantName,
+                          'dishes': dishName,
+                          'rating': rating,
+                        },
+                      );
+                      widget.onSave();
+                    },
+              child: const Text(
+                'DODAJ',
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
